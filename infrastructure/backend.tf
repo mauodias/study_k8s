67cb5c4f02ab5a22e4ac0edd3a88fced92033d99
Maurício Dias <mau.dias@gmail.com>
2021-07-24 15:31:45 +0200

    Adds Cloud Storage as backend to save tfstate

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "gcs" {
+    bucket = "kubepractices-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
 provider "google" {
   project = "kubepractices"
   region  = "europe-west4-a"
