d4e9e40507bc92c58d485a0dc132cf344d062f12
Maurício Dias <mau.dias@gmail.com>
2021-07-24 15:17:04 +0200

    First commit, creates v1 of infrastructure

---
+++ infrastructure/main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_instance" "controlnode" {
+  name         = "controlnode"
+  machine_type = "e2-small"
+  zone         = "europe-west4-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+}
